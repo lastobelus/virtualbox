5f9315da20d325d92c429afb8249c2f675746a42
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-23 01:26:00 -0700

    Added a VTblParent class to encapsulate that behaviour

---
+++ lib/virtualbox/ffi/vtbl_parent.rb
@@ -0,0 +1,22 @@
+module VirtualBox
+  module FFI
+    # A struct which behaves simply as a parent to a *_vtbl class, such
+    # as IMachine, which is a parent to IMachine_vtbl, and so on.
+    class VTblParent < ::FFI::Struct
+      class <<self
+        # Sets up the structure and instance method for accessing the
+        # vtbl of the struct. The `klass` specified as the only parameter
+        # specifies the name of the vtbl class.
+        def parent_of(klass)
+          # Define the layout
+          layout :vtbl, :pointer
+
+          # Create the instance method to access the vtbl
+          define_method :vtbl do
+            @_vtbl ||= klass.new(self, self[:vtbl])
+          end
+        end
+      end
+    end
+  end
+end
