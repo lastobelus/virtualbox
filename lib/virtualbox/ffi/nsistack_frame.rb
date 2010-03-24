3efa6adb06715087aff22884d383473ba162c2d7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-23 20:11:48 -0700

    Prefix the callbacks to avoid collisions

--- lib/virtualbox/ffi/nsistack_frame.rb
+++ lib/virtualbox/ffi/nsistack_frame.rb
@@ -3,14 +3,14 @@ module VirtualBox
     NS_ISTACKFRAME_IID_STR = "91d82105-7c62-4f8b-9779-154277c0ee90"
 
     # Callbacks for NSIStackFrame_vtbl
-    callback :GetLanguage, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetLanguageName, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetFilename, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetName, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetLineNumber, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetSourceLine, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetCaller, [:pointer, :pointer], NSRESULT_TYPE
-    callback :ToString, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetLanguage, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetLanguageName, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetFilename, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetName, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetLineNumber, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetSourceLine, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_GetCaller, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsisf_ToString, [:pointer, :pointer], NSRESULT_TYPE
 
     class NSIStackFrame < VTblParent
       parent_of :NSIStackFrame_vtbl
@@ -19,14 +19,14 @@ module VirtualBox
     class NSIStackFrame_vtbl < VTbl
       define_layout do
         member :nsisupports, NSISupports_vtbl
-        member :GetLanguage, :GetLanguage
-        member :GetLanguageName, :GetLanguageName
-        member :GetFilename, :GetFilename
-        member :GetName, :GetName
-        member :GetLineNumber, :GetLineNumber
-        member :GetSourceLine, :GetSourceLine
-        member :GetCaller, :GetCaller
-        member :ToString, :ToString
+        member :nsisf_GetLanguage, :nsisf_GetLanguage
+        member :nsisf_GetLanguageName, :nsisf_GetLanguageName
+        member :nsisf_GetFilename, :nsisf_GetFilename
+        member :nsisf_GetName, :nsisf_GetName
+        member :nsisf_GetLineNumber, :nsisf_GetLineNumber
+        member :nsisf_GetSourceLine, :nsisf_GetSourceLine
+        member :nsisf_GetCaller, :nsisf_GetCaller
+        member :nsisf_ToString, :nsisf_ToString
       end
     end
   end
