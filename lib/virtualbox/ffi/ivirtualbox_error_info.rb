fa1d8c6658f08fb88ab8f331a769c394a6430a99
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-23 20:48:35 -0700

    More function type prefixing to avoid collisions, specifically IVirtualBoxErrorInfo and NSIException

--- lib/virtualbox/ffi/ivirtualbox_error_info.rb
+++ lib/virtualbox/ffi/ivirtualbox_error_info.rb
@@ -3,11 +3,11 @@ module VirtualBox
     IVIRTUALBOXERRORINFO_IID_STR = "4b86d186-407e-4f9e-8be8-e50061be8725"
 
     # Callbacks for IVirtualBoxErrorInfo_vtbl
-    callback :GetResultCode, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetInterfaceID, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetComponent, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetText, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetNext, [:pointer, :pointer], NSRESULT_TYPE
+    callback :ivbei_GetResultCode, [:pointer, :pointer], NSRESULT_TYPE
+    callback :ivbei_GetInterfaceID, [:pointer, :pointer], NSRESULT_TYPE
+    callback :ivbei_GetComponent, [:pointer, :pointer], NSRESULT_TYPE
+    callback :ivbei_GetText, [:pointer, :pointer], NSRESULT_TYPE
+    callback :ivbei_GetNext, [:pointer, :pointer], NSRESULT_TYPE
 
     class IVirtualBoxErrorInfo < VTblParent
       parent_of :IVirtualBoxErrorInfo_vtbl
@@ -16,11 +16,14 @@ module VirtualBox
     class IVirtualBoxErrorInfo_vtbl < VTbl
       define_layout do
         member :nsiexception, NSIException
-        member :GetResultCode, :getter, PRInt32
-        member :GetInterfaceID, :getter, :unicode_string
-        member :GetComponent, :getter, :unicode_string
-        member :GetText, :getter, :unicode_string
-        member :GetNext, :getter, :IVirtualBoxErrorInfo
+
+        with_opts(:function_type_prefix => :ivbei_) do
+          member :GetResultCode, :getter, PRInt32
+          member :GetInterfaceID, :getter, :unicode_string
+          member :GetComponent, :getter, :unicode_string
+          member :GetText, :getter, :unicode_string
+          member :GetNext, :getter, :IVirtualBoxErrorInfo
+        end
       end
     end
   end
--- lib/virtualbox/ffi/nsiexception.rb
+++ lib/virtualbox/ffi/nsiexception.rb
@@ -3,16 +3,16 @@ module VirtualBox
     NS_IEXCEPTION_IID_STR = "f3a8d3b4-c424-4edc-8bf6-8974c983ba78"
 
     # Callbacks for NSIException_vtbl
-    callback :GetMessage, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetResult, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetName, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetFilename, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetLineNumber, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetColumnNumber, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetLocation, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetInner, [:pointer, :pointer], NSRESULT_TYPE
-    callback :GetData, [:pointer, :pointer], NSRESULT_TYPE
-    callback :ToString, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetMessage, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetResult, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetName, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetFilename, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetLineNumber, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetColumnNumber, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetLocation, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetInner, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_GetData, [:pointer, :pointer], NSRESULT_TYPE
+    callback :nsie_ToString, [:pointer, :pointer], NSRESULT_TYPE
 
     class NSIException < VTblParent
       parent_of :NSIException_vtbl
@@ -21,16 +21,19 @@ module VirtualBox
     class NSIException_vtbl < VTbl
       define_layout do
         member :nsisupports, NSISupports_vtbl
-        member :GetMessage, :getter, :unicode_string
-        member :GetResult, :getter, NSRESULT_TYPE
-        member :GetName, :getter, :unicode_string
-        member :GetFilename, :getter, :unicode_string
-        member :GetLineNumber, :getter, PRUint32
-        member :GetColumnNumber, :getter, PRUint32
-        member :GetLocation, :getter, :NSIStackFrame
-        member :GetInner, :getter, :NSIException
-        member :GetData, :getter, :NSISupports
-        member :ToString, :getter, :unicode_string
+
+        with_opts(:function_type_prefix => :nsie_) do
+          member :GetMessage, :getter, :unicode_string
+          member :GetResult, :getter, NSRESULT_TYPE
+          member :GetName, :getter, :unicode_string
+          member :GetFilename, :getter, :unicode_string
+          member :GetLineNumber, :getter, PRUint32
+          member :GetColumnNumber, :getter, PRUint32
+          member :GetLocation, :getter, :NSIStackFrame
+          member :GetInner, :getter, :NSIException
+          member :GetData, :getter, :NSISupports
+          member :ToString, :getter, :unicode_string
+        end
       end
     end
   end
