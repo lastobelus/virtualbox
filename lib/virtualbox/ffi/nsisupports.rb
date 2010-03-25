1261af2a7cfb558c8e8b3ede576a038a552dfd04
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-25 11:28:47 -0700

    NSISupports to VTbl

--- lib/virtualbox/ffi/nsisupports.rb
+++ lib/virtualbox/ffi/nsisupports.rb
@@ -1,18 +1,20 @@
 module VirtualBox
   module FFI
     # Callback types for nsISupports_vtbl
-    callback :QueryInterface, [:pointer, :pointer, :pointer], NSRESULT_TYPE
-    callback :AddRef, [:pointer], NSRESULT_TYPE
-    callback :Release, [:pointer], NSRESULT_TYPE
+    callback :nsis_QueryInterface, [:pointer, :pointer, :pointer], NSRESULT_TYPE
+    callback :nsis_AddRef, [:pointer], NSRESULT_TYPE
+    callback :nsis_Release, [:pointer], NSRESULT_TYPE
 
-    class NSISupports_vtbl < ::FFI::Struct
-      layout  :QueryInterface, :QueryInterface,
-              :AddRef, :AddRef,
-              :Release, :Release
+    class NSISupports_vtbl < VTbl
+      define_layout do
+        member :QueryInterface, :nsis_QueryInterface
+        member :AddRef, :nsis_AddRef
+        member :Release, :nsis_Release
+      end
     end
 
-    class NSISupports < ::FFI::Struct
-      layout :vtbl, :pointer # Pointer to NSISupports_vtbl
+    class NSISupports < VTblParent
+      parent_of :NSISupports_vtbl
     end
   end
 end
