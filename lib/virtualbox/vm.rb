e22c9229bb5d305a1046f94613f88ac8fbc3a63c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-03-28 16:52:10 -0700

    Interface setters for basic VM settings

--- lib/virtualbox/vm.rb
+++ lib/virtualbox/vm.rb
@@ -90,11 +90,11 @@ module VirtualBox
   class VM < AbstractModel
     attribute :uuid, :readonly => true, :interface_getter => :get_id
     attribute :name, :interface_getter => :get_name, :interface_setter => :set_name
-    attribute :ostype, :interface_getter => :get_os_type_id
-    attribute :description, :readonly => true, :interface_getter => :get_description
-    attribute :memory, :interface_getter => :get_memory_size
-    attribute :vram, :interface_getter => :get_vram_size
-    attribute :cpus, :interface_getter => :get_cpu_count
+    attribute :ostype, :interface_getter => :get_os_type_id, :interface_setter => :set_os_type_id
+    attribute :description, :readonly => true, :interface_getter => :get_description, :interface_setter => :set_description
+    attribute :memory, :interface_getter => :get_memory_size, :interface_setter => :set_memory_size
+    attribute :vram, :interface_getter => :get_vram_size, :interface_setter => :set_vram_size
+    attribute :cpus, :interface_getter => :get_cpu_count, :interface_setter => :set_cpu_count
     # TODO (in BIOS settings):
     # attribute :pae
     # attribute :hwvirtex
@@ -103,16 +103,16 @@ module VirtualBox
     # attribute :vtxvpid
     # attribute :acpi, :interface_getter => Proc.new { |m| m.get_bios_settings.get_acpi_enabled }
     # attribute :ioapic, :interface_getter => Proc.new { |m| m.get_bios_settings.get_ioapic_enabled }
-    attribute :accelerate3d, :interface_getter => :get_accelerate_3d_enabled
-    attribute :accelerate2dvideo, :interface_getter => :get_accelerate_2d_video_enabled
+    attribute :accelerate3d, :interface_getter => :get_accelerate_3d_enabled, :interface_setter => :set_accelerate_3d_enabled
+    attribute :accelerate2dvideo, :interface_getter => :get_accelerate_2d_video_enabled, :interface_setter => :set_accelerate_2d_video_enabled
     # TODO: BIOS Settings
     # attribute :biosbootmenu, :populate_key => :bootmenu
     # attribute :boot1
     # attribute :boot2
     # attribute :boot3
     # attribute :boot4
-    attribute :clipboard, :interface_getter => :get_clipboard_mode
-    attribute :monitorcount, :interface_getter => :get_monitor_count
+    attribute :clipboard, :interface_getter => :get_clipboard_mode, :interface_setter => :set_clipboard_count
+    attribute :monitorcount, :interface_getter => :get_monitor_count, :interface_setter => :set_monitor_count
     # TODO: USB, Audio, VRDP settings
     # attribute :usb
     # attribute :ehci
